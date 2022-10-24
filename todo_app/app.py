cd6e742393bb2eb3acec34780f6cda0b220bdaad
Terry Cheng <terrycheng925@gmail.com>
2022-10-24 23:24:33 +0100

    initial build

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask import render_template 
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import *
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +11,5 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template("index.html", items=items)
