a592e5cc3b6a1bbd30a76bb52364313fe1e4c6a3
Terry Cheng <terrycheng925@gmail.com>
2022-10-31 22:02:37 +0000

    final ex1 work

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template 
+from flask import request, redirect, url_for 
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import *
@@ -9,7 +10,17 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
+    
     items = get_items()
-    return render_template("index.html", items=items)
+
+    if request.method == "POST":
+        my_item = request.form["todo_item"] 
+        print(my_item)
+        items = add_item(my_item)
+        return redirect('/')
+    else:
+    # items = get_items()
+        return render_template("index.html", items=items)
+
